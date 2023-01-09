e9aeefebf12a7c5cfb8998f15beede24d90dd383
Seanbischi <sean.bischofberger@hotmail.com>
2023-01-09 19:57:56 +0100

    Basic Start of a test program for igcse test prepartion in 2023 .IGCSE 4 test coding .

---
+++ main.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+
+def main():
+    print(f'hi welcome to this side ')
+    print(f'may we ask waht is your name ')
+    print(f'we will calculate your avrig grad , the minimun that you can tip in is 1 the maximun is 6')
+    bad_grad = 3
+    midel_grad = 4
+    good_grad = 5
+    for grad_number in range(3):
+        print(f'waht is your grade ')
+
+
+if __name__ == '__main__':
+    main()
